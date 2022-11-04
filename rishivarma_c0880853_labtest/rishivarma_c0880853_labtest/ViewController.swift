3bf53066f08b6eff6f0816b3db1c8d69a4894731
Rishi Varma Kalidindi <rishivarma.lc.dev@gmail.com>
2022-11-04 18:44:35 -0400

    Project Created

---
+++ rishivarma_c0880853_labtest/rishivarma_c0880853_labtest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rishivarma_c0880853_labtest
+//
+//  Created by RISHI VARMA on 2022-11-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
