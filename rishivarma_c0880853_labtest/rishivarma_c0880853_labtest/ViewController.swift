4db6e09c1ee2829cf9fc726f4886aacaae581e7a
Rishi Varma Kalidindi <rishivarma.lc.dev@gmail.com>
2022-11-04 19:26:26 -0400

    Added links of Interface elements in the code

--- rishivarma_c0880853_labtest/rishivarma_c0880853_labtest/ViewController.swift
+++ rishivarma_c0880853_labtest/rishivarma_c0880853_labtest/ViewController.swift
@@ -7,8 +7,26 @@
 
 import UIKit
 
+struct Flag {
+    let index : Int
+    let timeDiff : Int
+    let flagTime : Int
+}
+
+enum State {
+    case Play
+    case Pause
+    case Stop
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stopWatch: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var buttonFlag: UIButton!
+    @IBOutlet weak var buttonPause: UIButton!
+    @IBOutlet weak var tableFlags: UITableView!
+    @IBOutlet weak var buttonFlagPauseStack: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
